1bd3a5a87c1dedd4ece2a8afe91a21e7c8b4ceae
Joeyyyyyyy <jjjj8912@gmail.com>
2024-12-11 04:07:53 +0530

    added to changelog

--- code.py
+++ code.py
@@ -41,7 +41,7 @@ Developer Notes:
 ##(3.7.1) Made the Open File feature more secure
 ##(3.7.2) Added Object deletion for memory optimization
 ##(3.7.3) Patched the docx extraction feature
-##(3.7.4) Created custom dialog boxes
+##(3.7.4) Created custom dialog boxes and improved awlist
 ##(3.8.1) Improved the report generation
 """
 
